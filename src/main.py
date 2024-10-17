9b90d743be7528a302bacdb0d3c8d45f69d83616
Utente <cossu016@gmail.com>
2024-10-17 11:08:15 +0200

    modificato

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 from splitters import split_data
 from classifiers import NMC
 
-data = pd.read_csv("data/mnist_data.csv")
+data = pd.read_csv("../data/mnist_data.csv") #i .. servono per andare nella cartella padre
 data = np.array(data)
 print(data.shape)
 print(type(data))
